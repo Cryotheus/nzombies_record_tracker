fdf96d6c2d6bf488466ad0b5411f1a9ac39e8e9e
wised <14439112+Cryotheus@users.noreply.github.com>
2020-04-05 16:10:52 -0600

    Added swedish translations file (whoops)

---
+++ lua/nz_record_tracker_lang/se.lua
@@ -0,0 +1,12 @@
+--translated by FrenZy#7752 from GFL Teams on Discord
+return {
+	["CONGRATS_COMMAND"]	= "Ger dig konfetti och festliga ljud. Dock endast för din klient.",
+	["CONGRATS_GLOBAL"]		= "Grattis, du slog serverns rekord för högsta nådda rundan!",
+	["CONGRATS_MAP"]		= "Grattis! Du slog kartans rekord för högsta nådda rundan! Försök nu att slå serverns rekord på %u.",
+	["GUI_BUTTON"]			= "Rekord spårarens ledartavla",
+	["GUI_ENTRY"]			= "%s - Runda %u",
+	["GUI_ENTRY_MAP"]		= "%s\n\n%u elimineringar",
+	["GUI_ENTRY_MAP_LEFT"]	= "%s\nKvar på rundan %u\n%u elimineringar",
+	["GUI_TITLE"]			= "Ledartavlan",
+	["GUI_TITLE_MAP"]		= "Ledartavlan - %s"
+}
